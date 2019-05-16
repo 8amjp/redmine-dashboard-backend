5e19f6e87135c98e678719751a268c00711a3c03
8amjp <wakeup@8am.jp>
2019-05-16 19:37:40 +0900

    0.0.1

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('redmine-dashboard-backend');
 });
+
 router.get('/cache-minutely', function(req, res, next) {
   require('../jobs/cache-minutely')();
   res.send('cache-minutely');
@@ -16,5 +17,13 @@ router.get('/cache-daily', function(req, res, next) {
   require('../jobs/cache-daily')();
   res.send('cache-daily');
 });
+
+router.get('/init', function(req, res, next) {
+  require('../jobs/cache-minutely')();
+  require('../jobs/cache-hourly')();
+  require('../jobs/cache-daily')();
+  res.send('init');
+});
+
 
 module.exports = router;
